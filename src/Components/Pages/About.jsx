424ddeedc539dfcd657d5c2fe95eb39a145cb8c9
Yes-said <bobsaid358@gmail.com>
2024-04-17 16:39:55 +0300

    pages updated

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <div>
+      School Name:Kenya Institute Of Mass Communication<br/><br/>
+Address: Kenya Institute of Mass Communication<br/>
+P.O. Box 42422 – 00100 Uholo Road,<br/>
+Off Mombasa Road
+Nairobi
+Nairobi Kenya<br/><br/>
+Telephone: +254-020-699 7000,. Cell: 0708 262 895<br/>
+Email:  Info@kimc.ac.ke<br/>
+Website:  http://www.kimc.ac.ke<br/><br/><br/>
+</div>
+        </div>
   )
 }
 
