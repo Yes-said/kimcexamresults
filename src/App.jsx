04e792ed49956eaf6bb09a9b8b6eec5bdd1d0fdf
Yes-said <bobsaid358@gmail.com>
2024-04-19 20:21:20 +0300

    sidebar,pages navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,27 @@ import About from './Components/Pages/About';
 import Contacts from './Components/Pages/Contacts';
 import Documentation from './Components/Pages/Documentation';
 import Home from './Components/Pages/Home';
+import Sidebar from './Components/Sidebar';
+import Courses from './Components/Pages/Courses';
+import Results from './Components/Pages/Results';
+import Students from './Components/Pages/Students';
 function App(){
 return (
-<div className="App">
+<div className="w-full h-screen bg-back
+object-cover flex items-center">
   <Navbar/>
   <Routes>
   <Route path="/" element={<Home/>}/>
 <Route path="/About" element={<About/>}/>
 <Route path="/Contacts" element={<Contacts/>}/>
 <Route path="/Documentation" element={<Documentation/>}/>
+  </Routes>
+<Sidebar/>
+<Routes>
+  <Route path="/" element={<Home/>}/>
+<Route path="/Courses" element={<Courses/>}/>
+<Route path="/Results" element={<Results/>}/>
+<Route path="/Students" element={<Students/>}/>
   </Routes>
   </div>
   );
