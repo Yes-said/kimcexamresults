aa4b75c3e209271325f3068bfad40740dbfc8975
Yes-said <bobsaid358@gmail.com>
2024-04-19 21:29:01 +0300

    sidebar,pages updatded

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ import About from './Components/Pages/About';
 import Contacts from './Components/Pages/Contacts';
 import Documentation from './Components/Pages/Documentation';
 import Home from './Components/Pages/Home';
-import Sidebar from './Components/Sidebar';
-import Courses from './Components/Pages/Courses';
-import Results from './Components/Pages/Results';
-import Students from './Components/Pages/Students';
+
 import Footer from './Components/Footer';
 function App(){
 return (
@@ -20,13 +17,6 @@ object-cover flex items-center">
 <Route path="/About" element={<About/>}/>
 <Route path="/Contacts" element={<Contacts/>}/>
 <Route path="/Documentation" element={<Documentation/>}/>
-  </Routes>
-<Sidebar/>
-<Routes>
-  <Route path="/" element={<Home/>}/>
-<Route path="/Courses" element={<Courses/>}/>
-<Route path="/Results" element={<Results/>}/>
-<Route path="/Students" element={<Students/>}/>
   </Routes>
   <Footer/>
   </div>
